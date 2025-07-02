2d01f2abe3a2ad43ed3df9d69e3650036a3d3ad8
jay becina <jaybecina@gmail.com>
2025-07-03 03:27:16 +0800

    Initialized Native Wind and Gluestack manual setup

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,23 @@
 import { FlatList, Text, View } from "react-native";
 import products from "@/assets/products.json";
 import ProductListItem from "@/components/ProductListItem";
+import { Button, ButtonText } from "@/components/ui/button";
 
 export default function HomeScreen() {
   return (
-    <View>
-      <FlatList
-        data={products}
-        renderItem={({ item }) => <ProductListItem item={item} />}
-      />
+    // <View>
+    //   <FlatList
+    //     data={products}
+    //     renderItem={({ item }) => <ProductListItem item={item} />}
+    //   />
+    // </View>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-xl font-bold text-blue-500">
+        Welcome to Nativewind!
+      </Text>
+      <Button size="md" variant="solid" className="bg-red-500">
+        <ButtonText>Gluestack Button</ButtonText>
+      </Button>
     </View>
   );
 }
